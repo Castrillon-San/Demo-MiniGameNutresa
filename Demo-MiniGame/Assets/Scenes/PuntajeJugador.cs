2e4f0cb40996d99bf245445490507ea92c4c1eea
Santiago <60115630+Castrillon-San@users.noreply.github.com>
2023-03-02 12:00:49 -0500

    StableDemo

---
+++ Demo-MiniGame/Assets/Scenes/PuntajeJugador.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Proyecto26;
+using UnityEngine.SceneManagement;
+
+public class PuntajeJugador : MonoBehaviour
+{
+    public TMP_InputField textoNombre;
+    public static string nombreJugador;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Enviar()
+    {
+        if (textoNombre.text == "") return;
+        nombreJugador = textoNombre.text;
+        PlayerPrefs.SetString("nombreJugador", nombreJugador);
+        EnviarPorPost();
+        NextScene();
+    }
+
+    public void EnviarPorPost()
+    {
+        Usuario usuario = new Usuario();
+        RestClient.Post("https://fir-minigame-default-rtdb.firebaseio.com/.json", usuario);
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Demo-MiniGame/Assets/ScoreCounter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    [SerializeField] TMP_Text counterText;
+    public int counterPoints = 0;
+
+    public static ScoreCounter Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    void Start()
+    {
+        //counterText = gameObject.GetComponent<TMP_Text>();
+    }
+
+    public void UpdateScore()
+    {
+        counterPoints += 1;
+        counterText.text = $"{counterPoints}";
+    }
+}
