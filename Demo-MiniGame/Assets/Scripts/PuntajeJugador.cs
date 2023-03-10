4df1ee6b3c86771f3c8d570103ca544106e8256b
Santiago Castrillón Gómez <santiago.castrillong@upb.edu.co>
2023-03-10 16:11:08 -0500

    Cambios en oficina

--- Demo-MiniGame/Assets/Scripts/PuntajeJugador.cs
+++ Demo-MiniGame/Assets/Scripts/PuntajeJugador.cs
@@ -1,32 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using Proyecto26;
 using UnityEngine.SceneManagement;
 
 public class PuntajeJugador : MonoBehaviour
 {
     [SerializeField] GameObject levelSelectorCanva;
     [SerializeField] GameObject formCanva;
-    public TMP_InputField textoNombre;
+    [SerializeField] TMP_InputField textoNombre;
+    [SerializeField] TMP_InputField textoApellido;
     public static string nombreJugador;
 
     public void Enviar()
     {
-        if(textoNombre.text == "") return;
-        nombreJugador = textoNombre.text;
+        if(textoNombre.text == "" || textoApellido.text == "") return;
+        nombreJugador = textoNombre.text + " " + textoApellido.text;
         PlayerPrefs.SetString("nombreJugador", nombreJugador);
-        //EnviarPorPost();
         formCanva.SetActive(false);
         levelSelectorCanva.SetActive(true);
     }
-
-    //public void EnviarPorPost()
-    //{
-    //    Usuario usuario = new Usuario();
-    //    RestClient.Post("https://fir-minigame-default-rtdb.firebaseio.com/.json", usuario);
-    //}
 
 
     public void NextScene(int sceneIndex)
